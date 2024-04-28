ae160565ad5c54c8568895f4879985558b4e8f20
Cleyton-sousa <cleyton.santos@b2bti.com>
2024-04-28 18:44:56 -0300

    add doc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,24 @@
 import { Controller, Delete, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { MinioService } from './minio/minio.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller("/image")
+@ApiTags('Image')
 export class AppController {
   constructor(private readonly minioService: MinioService) {}
 
   @Post("/save")
   @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: 'Upload an image file' })
+  @ApiResponse({ status: 200, description: 'image url' })
   saveImage(@UploadedFile() file: Express.Multer.File) {
     return this.minioService.uploadFile(file);
   }
 
   @Delete("/delete/:fileName")
+  @ApiOperation({ summary: 'Delete an image file' })
+  @ApiResponse({ status: 200, description: 'Image deleted successfully' })
   deleteImage(@Param("fileName") filename: string){
     return this.minioService.deleteFile(filename);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { config } from 'dotenv';
 
 config
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Images API')
+  .setDescription('The Images API description')
+  .setVersion('1.0')
+  .addTag('images')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(process.env.PORT || 9090, process.env.HOST || "0.0.0.0", () => {
     console.log(`Server up on PORT: ${process.env.PORT || 9090}, and host: ${process.env.HOST || "0.0.0.0"}`)
   });
