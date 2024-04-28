11f684b0cc6aef491bb26d08aa3590332f2e2a48
Cleyton-sousa <cleyton.santos@b2bti.com>
2024-04-28 18:36:46 -0300

    v1

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { config } from 'dotenv';
 config
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 9090, () => console.log("Service online on: ", process.env.PORT || 9090));
+  await app.listen(process.env.PORT || 9090, process.env.HOST || "0.0.0.0", () => {
+    console.log(`Server up on PORT: ${process.env.PORT || 9090}, and host: ${process.env.HOST || "0.0.0.0"}`)
+  });
 }
 bootstrap();
